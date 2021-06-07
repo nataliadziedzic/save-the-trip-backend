b365a14dc82ddb7d7d48ab0dcf48e1bebaf131b2
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-07 12:54:54 +0200

    Connect to a database

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const mysql = require('mysql2')
+
 const app = express()
-const port = 5000
+app.use(express.json())
 
 const cors = require('cors')
 const allowedOrigins = ['http://localhost:3000']
@@ -13,6 +15,18 @@ app.use(
   })
 )
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Running at http://localhost:${port}`)
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+})
+
+db.connect((err) => {
+  if (err) throw err
+  console.log('Connected to a database!')
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Running`)
 })
