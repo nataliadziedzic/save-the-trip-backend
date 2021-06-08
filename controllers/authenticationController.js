9a15bbb8719cc69638177de440de215b93ead086
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-08 09:37:25 +0200

    Add login controller

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,4 +1,4 @@
-const getDb = require('../database/database')
+const getDb = require('../config/database')
 const bcrypt = require('bcryptjs')
 
 exports.registrationController = async (req, res) => {
@@ -16,6 +16,24 @@ exports.registrationController = async (req, res) => {
         email,
       ])
       res.status(201).json({ message: 'User created.' })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.loginController = async (req, res) => {
+  const { email, password } = req.body
+  const db = await getDb()
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide an email and a password.' })
+    }
+    const [rows, fields] = await db.query('SELECT * FROM users WHERE email = ?', [email])
+    if (rows.length === 0) return res.status(404).json({ message: "Account doesn't exist" })
+    if (rows[0].email === email) {
+      const isPasswordCorrect = await bcrypt.compare(password, rows[0].password)
+      isPasswordCorrect ? res.status(200) : res.status(401).json({ message: 'Invalid password' })
     }
   } catch (error) {
     console.log(error)
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { registrationController } = require('../controllers/authenticationController')
+const { registrationController, loginController } = require('../controllers/authenticationController')
 const router = express.Router()
 
 router.post('/register', async (req, res) => registrationController(req, res))
+router.post('/login', async (req, res) => loginController(req, res))
 
 module.exports = router
