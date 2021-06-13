0c1d7273aa13989eb190682907a949918d84b638
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-13 16:43:41 +0200

    GET single trip

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -14,27 +14,47 @@ exports.getTripsController = async (req, res) => {
   }
 }
 
+exports.getSingleTripController = async (req, res) => {
+  res.json(res.trip)
+}
+
 exports.createTripController = async (req, res) => {
   const db = await getDb()
   const { title, description, user_id, start_date, img } = req.body
   try {
-    if (!user_id || !title || !description || !start_date)
-      return res.status(400).json({ message: 'Bad request' })
-    const newTrip = await db.query(
-      'INSERT INTO trips (title, description, user_id, start_date) VALUES (?, ?, ?, ?)',
-      [title, description, user_id, start_date]
-    )
-    const documentsListForTrip = await db.query(
-      'INSERT INTO documents_lists (trip_id, user_id) VALUES (?, ?)',
-      [newTrip[0].insertId, user_id]
-    )
+    if (!user_id || !title || !description || !start_date) return res.status(400).json({ message: 'Bad request' })
+    const newTrip = await db.query('INSERT INTO trips (title, description, user_id, start_date) VALUES (?, ?, ?, ?)', [
+      title,
+      description,
+      user_id,
+      start_date,
+    ])
+    const documentsListForTrip = await db.query('INSERT INTO documents_lists (trip_id, user_id) VALUES (?, ?)', [
+      newTrip[0].insertId,
+      user_id,
+    ])
     const selectNewTrip = await db.query('SELECT * FROM trips WHERE id = ?', [newTrip[0].insertId])
-    res
-      .status(200)
-      .json({ trip: selectNewTrip[0][0], documents_list_id: documentsListForTrip[0].insertId })
+    res.status(200).json({ trip: selectNewTrip[0][0], documents_list_id: documentsListForTrip[0].insertId })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: error.message })
     process.exit(1)
   }
+}
+
+exports.getTrip = async (req, res, next) => {
+  console.log('get by id')
+  const db = await getDb()
+  let trip
+  try {
+    const dbTrips = await db.query('SELECT * FROM trips WHERE id = ?', [req.params.id])
+    if (dbTrips[0].length === 0) {
+      return res.status(404).json({ message: 'Cannot find trip' })
+    }
+    trip = dbTrips[0][0]
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+  res.trip = trip
+  next()
 }
--- routes/trips.js
+++ routes/trips.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const { getTripsController, createTripController } = require('../controllers/tripsController')
+const { getTripsController, getSingleTripController, createTripController, getTrip } = require('../controllers/tripsController')
 
 router.get('/user/:userId/trips', passport.authenticate('jwt', { session: false }), (req, res) =>
   getTripsController(req, res)
 )
-router.post('/trips', passport.authenticate('jwt', { session: false }), (req, res) =>
-  createTripController(req, res)
+
+router.get('/trip/:id', passport.authenticate('jwt', { session: false }), getTrip, (req, res) =>
+  getSingleTripController(req, res)
 )
+
+router.post('/trips', passport.authenticate('jwt', { session: false }), (req, res) => createTripController(req, res))
 
 module.exports = router
