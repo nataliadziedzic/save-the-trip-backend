75b6f1aba306b831a99190751856aff4c640dfa9
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-14 10:05:22 +0200

    Delete refresh token on logout

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -64,19 +64,29 @@ exports.loginController = async (req, res) => {
   }
 }
 
+exports.logoutController = async (req, res) => {
+  const userId = req.params.id
+  const db = await getDb()
+  try {
+    await db.query(`UPDATE users SET refreshToken = ? WHERE id = ?`, [null, userId])
+    res.sendStatus(204)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+    process.exit(1)
+  }
+}
+
 exports.refreshController = async (req, res) => {
   const db = await getDb()
   const { refreshToken } = req.body
   try {
-    if (!refreshToken) return res.status(401).json({ message: 'Unauthorized' })
-    if (!(await db.query('SELECT * FROM users WHERE refreshToken = ?', [refreshToken])))
-      return res.status(403)
+    if (!refreshToken) return res.status(403).json({ message: 'Forbidden' })
+    const userWithToken = await db.query('SELECT * FROM users WHERE refreshToken = ?', [refreshToken])
+    if (userWithToken[0].length === 0) return res.status(401).json({message: 'Unauthorized'})
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, user) => {
       if (error) return res.status(403).json({ message: 'Forbidden' })
       const accessToken = generateAccessToken(user)
-      return res
-        .status(200)
-        .json({ accessToken: accessToken, user: user.id, username: user.username })
+      return res.status(200).json({ accessToken: accessToken, user: user.id, username: user.username })
     })
   } catch (error) {
     res.status(500).json({ message: error.message })
@@ -86,9 +96,9 @@ exports.refreshController = async (req, res) => {
 
 const generateAccessToken = user => {
   return jwt.sign({ id: user.id, username: user.username }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: '30m',
+    expiresIn: '10m',
   })
 }
 const generateRefreshToken = user => {
-  return jwt.sign({ id: user.id, username: user.username }, process.env.REFRESH_TOKEN_SECRET)
+  return jwt.sign({ id: user.id, username: user.username }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'})
 }
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { registrationController, loginController, refreshController } = require('../controllers/authenticationController')
+const { registrationController, loginController, refreshController, logoutController } = require('../controllers/authenticationController')
 const router = express.Router()
 
 router.post('/register', async (req, res) => registrationController(req, res))
 router.post('/login', async (req, res) => loginController(req, res))
+router.get('/logout/:id', async (req, res) => logoutController(req, res))
 router.post('/refresh', async (req, res) => refreshController(req, res))
 
 module.exports = router
