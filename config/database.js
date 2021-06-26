8ad61ab2d645533030117a9b0597ca87bb25f197
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-26 14:44:06 +0200

    Add trycarch block to mysql connection

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 async function getDb() {
   const mysql = require('mysql2/promise')
-  const db = await mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-  })
-  return db
+  try {
+    const db = await mysql.createConnection({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+    })
+    return db
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 
 module.exports = getDb
